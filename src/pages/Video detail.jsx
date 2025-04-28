36c03c62e16245329ce39d81333e477a7de31a04
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-04-28 11:05:37 +0530

    Create Video detail.jsx

---
+++ src/pages/Video detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function VideoDetail() {
+  return (
+    <div style={{ flex: 1, padding: "20px" }}>
+      <h2>Video Title Here</h2>
+      <p>Video details and description.</p>
+    </div>
+  );
+}
+
+export default VideoDetail;
