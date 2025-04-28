18ccf9497ae2f74aa04ad533d409bbe79752ed4c
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-04-28 11:12:17 +0530

    Create Profile.jsx

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div style={{ flex: 1, padding: "20px" }}>
+      <h2>Your Profile</h2>
+      <p>Manage your videos, likes, and subscriptions here.</p>
+    </div>
+  );
+}
+
+export default Profile;
