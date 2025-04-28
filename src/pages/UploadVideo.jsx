f339d10264b5ede8ba750d5391a0aa298d3fe56d
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-04-28 11:11:51 +0530

    Create UploadVideo.jsx

---
+++ src/pages/UploadVideo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UploadVideo() {
+  return (
+    <div style={{ flex: 1, padding: "20px" }}>
+      <h2>Upload Your Video</h2>
+      <input type="file" />
+    </div>
+  );
+}
+
+export default UploadVideo;
