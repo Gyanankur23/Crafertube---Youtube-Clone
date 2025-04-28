2784b3cce3a600a9664c98d99d5f87cb02ef9768
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-04-28 11:05:07 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoGrid from "../components/VideoGrid";
+
+function Home() {
+  return (
+    <div style={{ flex: 1 }}>
+      <VideoGrid />
+    </div>
+  );
+}
+
+export default Home;
